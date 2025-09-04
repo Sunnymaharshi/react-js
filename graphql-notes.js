eef7447b2e8c7ccc7c6ca71904a54c7d52b26eb3
Sunnymaharshi <maharshireddythangasani@gmail.com>
2025-09-04 08:13:59 +0530

    graph ql notes

---
+++ graphql-notes.js
@@ -0,0 +1,17 @@
+/*
+    GraphQL 
+        apollo client
+            used at client side to call GraphQL apis 
+            create client & pass it to ApolloProvider in App component
+            gql is used write queries & mutations
+            useQuery hook is used to run the queries 
+                gives error,loading & data 
+            useMutation hook is used to run the mutations
+                given mutation function and error
+        apollo server 
+            used in server to create GraphQL queries & mutations 
+            typedefs 
+                to define types for queries & mutations 
+            resolvers 
+                actions for queries & mutations
+*/
